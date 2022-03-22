9242588c1298a346c1b743ef33f2fe6fe827f034
jeonyoungho <yhjun1000@gmail.com>
2022-03-22 19:29:55 +0900

    HtmlWebpackPlugin 실습 내용 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 // const MyWebpackPlugin = require('./my-webpack-plugin');
 const webpack = require('webpack');
 // const childProcess = require('child_process');
-const banner = require("./banner.js")
+const banner = require("./banner.js");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = { // es6의 모듈 시스템은 아니고 node 의 모듈 시스템이다.(CommonJS)
     mode: 'development',
@@ -26,7 +27,7 @@ module.exports = { // es6의 모듈 시스템은 아니고 node 의 모듈 시�
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'url-loader', // fallback 기본값이 file-loader이기 때문에 20kb 이상은 file-loader 가 처리한다.
                 options: {
-                    publicPath: './dist/', // 파일 로더가 처리하는 파일을 모듈로 사용했을때 경로 앞에 추가되는 문자열이다, 파일을 호출하는 측에선 dist 를 붙이고 파일을 호출할 것이다.
+                    // publicPath: './dist/', // 파일 로더가 처리하는 파일을 모듈로 사용했을때 경로 앞에 추가되는 문자열이다, 파일을 호출하는 측에선 dist 를 붙이고 파일을 호출할 것이다.
                     name: '[name].[ext]?[hash]', // 파일 로더가 output에 복사할때 사용하는 파일 이름, [원본 파일명].[확장자]?해쉬값
                     limit: 20000, // 20kb 미만의 파일은 url-loader 로 해서 base64로 변환한다.(파일을 javascript 문자열로 변환) 만약 2kb 이상일 경우 file-loader가 실행하도록 한다.
                 }
@@ -35,6 +36,16 @@ module.exports = { // es6의 모듈 시스템은 아니고 node 의 모듈 시�
     },
     plugins: [
         // new MyWebpackPlugin(),
-        new webpack.BannerPlugin(banner)
+        new webpack.BannerPlugin(banner),
+        new webpack.DefinePlugin({
+            TWO: JSON.stringify('1+1'),
+            'api.domain': JSON.stringify('http://dev.api.domain.com')
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+            }
+        })
     ]
 }
