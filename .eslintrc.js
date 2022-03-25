51801f36c6c429bcf6ccb5b6fe97927848171750
jeonyoungho <yhjun1000@gmail.com>
2022-03-25 18:22:11 +0900

    prettier 실습 내용 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        'eslint-config-prettier' // eslint:reccomended 설정에 있는 규칙들 중에 prettier와 겹치는게 있으면 다 꺼버리는 설정
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -12,6 +15,10 @@ module.exports = {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
+    plugins: [
+        "prettier"
+    ],
     "rules": {
+        "prettier/prettier": "error"
     }
 };
